004238b91f9de8a0954d17c335c8b3f7e1f5eff8
sgtlaggy <8661717+sgtlaggy@users.noreply.github.com>
2024-09-03 14:10:10 -0700

    add configuration option

--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,9 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 
 using EFT;
 using BepInEx;
+using BepInEx.Configuration;
 using SPT.Reflection.Patching;
 
 namespace ShoulderSwapFix
@@ -9,13 +11,39 @@ namespace ShoulderSwapFix
     [BepInPlugin("com.sgtlaggy.shoulderswapfix", "ShoulderSwapFix", "1.0.0")]
     public class Plugin : BaseUnityPlugin
     {
+        public static ConfigEntry<bool> Enabled { get; set; }
+
         private void Awake()
         {
-            new HandInteractionPatch().Enable();
-            new InventoryPatch().Enable();
-            new KnifePatch().Enable();
-            new RangefinderPatch().Enable();
-            new RadioTransmitterPatch().Enable();
+            Enabled = Config.Bind("General", "Enabled", true, "Keep weapon in left shoulder during most interactions.");
+            Enabled.SettingChanged += TogglePatches;
+            TogglePatches(Enabled, EventArgs.Empty);
+        }
+
+        private void TogglePatches(object sender, EventArgs e)
+        {
+            var setting = sender as ConfigEntry<bool>;
+            if (setting == null)
+            {
+                return;
+            }
+
+            if (setting.Value)
+            {
+                new HandInteractionPatch().Enable();
+                new InventoryPatch().Enable();
+                new KnifePatch().Enable();
+                new RangefinderPatch().Enable();
+                new RadioTransmitterPatch().Enable();
+            }
+            else
+            {
+                new HandInteractionPatch().Disable();
+                new InventoryPatch().Disable();
+                new KnifePatch().Disable();
+                new RangefinderPatch().Disable();
+                new RadioTransmitterPatch().Disable();
+            }
         }
     }
 
